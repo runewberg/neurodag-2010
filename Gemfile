6431914cd26d33587025a86ee346077760071d0f
Rune Berg <rune2earth@gmail.com>
2010-08-18 22:03:49 +0200

    rails3 upgrade plugin run

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0.rc'
+gem 'mysql'
+# gem "ambethia-smtp-tls", :require => "smtp-tls"
+# gem "scrapi"  
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Neurodag2010::Application.config.secret_token = 'fb62a88a83496d7be164ae8c0df00a9aaad8078184875ec955edfb88dad1108a77d4572fe2edc59a6ca24e57cde53b6f9501c76b8b8f66221d4c7c918533a8ca'
